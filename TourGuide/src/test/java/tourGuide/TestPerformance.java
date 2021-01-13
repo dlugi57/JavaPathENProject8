d1fd5722cc18e4ba2d70b6fb78eca81f979cb558
dlugi57 <dlugoszpeter@gmail.com>
2021-01-13 22:42:34 +0100

    CLEAN

--- TourGuide/src/test/java/tourGuide/TestPerformance.java
+++ TourGuide/src/test/java/tourGuide/TestPerformance.java
@@ -42,38 +42,34 @@ public class TestPerformance {
      *          assertTrue(TimeUnit.MINUTES.toSeconds(20) >= TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
      */
 
-    //@Ignore
     @Test
-    public void highVolumeTrackLocation() throws InterruptedException {
+    public void highVolumeTrackLocation() throws InterruptedException, ExecutionException {
         GpsUtil gpsUtil = new GpsUtil();
         RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
         // Users should be incremented up to 100,000, and test finishes within 15 minutes
-        InternalTestHelper.setInternalUserNumber(1000);
+        InternalTestHelper.setInternalUserNumber(100000);
         TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
+        tourGuideService.tracker.stopTracking();
 
         List<User> allUsers = new ArrayList<>();
         allUsers = tourGuideService.getAllUsers();
+        List<CompletableFuture> futures = new ArrayList();
 
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
 
-        // TODO: 08/01/2021 can i just send more at once ?
-		//allUsers.parallelStream().forEach(u->tourGuideService.trackUserLocation(u));
+        allUsers.forEach(u -> futures.add(CompletableFuture.runAsync(() -> {
+            tourGuideService.trackUserLocation(u);
+        })));
+        CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+        combinedFuture.get();
 
-
-		tourGuideService.trackListOfUserLocation(allUsers);
-/*		for(User user : allUsers) {
-			tourGuideService.trackUserLocation(user);
-		}*/
         stopWatch.stop();
-        tourGuideService.tracker.stopTracking();
 
         System.out.println("highVolumeTrackLocation: Time Elapsed: " + TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()) + " seconds.");
-
         assertTrue(TimeUnit.MINUTES.toSeconds(15) >= TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
     }
 
-    //@Ignore
     @Test
     public void highVolumeGetRewards() throws ExecutionException, InterruptedException {
         GpsUtil gpsUtil = new GpsUtil();
@@ -82,65 +78,30 @@ public class TestPerformance {
         // Users should be incremented up to 100,000, and test finishes within 20 minutes
         InternalTestHelper.setInternalUserNumber(100000);
         TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
+        tourGuideService.tracker.stopTracking();
+
         List<CompletableFuture> futures = new ArrayList();
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
-
-        // TODO: 07/01/2021 you have to be kidding me is only that ??
-        //tourGuideService.tracker.stopTracking();
-
 
         Attraction attraction = gpsUtil.getAttractions().get(0);
         List<User> allUsers = new ArrayList<>();
         allUsers = tourGuideService.getAllUsers();
 
-
         allUsers.forEach(u -> u.addToVisitedLocations(new VisitedLocation(u.getUserId(),
 				attraction, new Date())));
-        tourGuideService.tracker.stopTracking();
+
         allUsers.forEach(u -> futures.add(CompletableFuture.runAsync(() -> {
             rewardsService.calculateRewards(u);
         })));
 
-
         CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(futures.toArray(new CompletableFuture[allUsers.size()]));
         combinedFuture.get();
 
-
-/*
-        allUsers.parallelStream().forEach(user -> {
-            user.addToVisitedLocations(new VisitedLocation(user.getUserId(), attraction, new Date()));
-            rewardsService.calculateRewards(user);
-            assertTrue(user.getUserRewards().size() > 0);
-        });*/
-
-        //ExecutorService executorService = Executors.newFixedThreadPool(1000);
-
-		/*for(User user : allUsers) {
-
-			Runnable runnableTask = () -> {
-				user.addToVisitedLocations(new VisitedLocation(user.getUserId(), attraction, new Date()));
-				rewardsService.calculateRewards(user);
-				assertTrue(user.getUserRewards().size() > 0);
-			};
-
-			executorService.execute(runnableTask);
-		}
-		executorService.shutdown();
-		executorService.awaitTermination(25, TimeUnit.MINUTES);
-		*/
-
-
         stopWatch.stop();
-        //tourGuideService.tracker.stopTracking();
 
         System.out.println("highVolumeGetRewards: Time Elapsed: " + TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()) + " seconds.");
         assertTrue(TimeUnit.MINUTES.toSeconds(20) >= TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
-
-
-
-
-
     }
 
 }
