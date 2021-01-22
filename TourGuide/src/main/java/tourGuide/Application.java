728882a3edc0f78ec4c2c6b76a58efc45e2b6907
dlugi57 <dlugoszpeter@gmail.com>
2021-01-23 00:14:12 +0100

    RELEASE configuration

--- TourGuide/src/main/java/tourGuide/Application.java
+++ TourGuide/src/main/java/tourGuide/Application.java
@@ -2,8 +2,13 @@ package tourGuide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients("tourGuide")
 public class Application {
 
     public static void main(String[] args) {
