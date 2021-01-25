21ef64fee097559d85e8fc7c955d94388c14554d
dlugi57 <dlugoszpeter@gmail.com>
2021-01-25 21:50:08 +0100

    DEV gps proxy initialization

---
+++ TourGuide/src/main/java/tourGuide/proxies/GpsUtilProxy.java
@@ -0,0 +1,22 @@
+package tourGuide.proxies;
+
+import gpsUtil.location.Attraction;
+import gpsUtil.location.VisitedLocation;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.UUID;
+
+@FeignClient(name = "gps", url = "localhost:8081")
+
+public interface GpsUtilProxy {
+
+    @GetMapping("/gps/getUserLocation")
+    VisitedLocation getUserLocation(@RequestParam("userId") final UUID userId);
+
+    @GetMapping("/gps/getAttractions")
+    List<Attraction> getAttractions();
+
+}
